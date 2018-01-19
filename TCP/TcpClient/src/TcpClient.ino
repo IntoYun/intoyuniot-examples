8b57712aa8ac032a0626028a15fa589a6e3e750e
chenkaiyao <chenkaiyao@molmc.com>
2018-01-19 14:14:48 +0800

    1. 更新tcp相关示例

--- TCP/TcpClient/src/TcpClient.ino
+++ TCP/TcpClient/src/TcpClient.ino
@@ -1,15 +1,22 @@
+/*
+    TCP client
+    创建一个Tcp客户端, 显示tcp服务器端发送过来的数据。
+*/
+
+// 手动连接IntoYun云平台
+SYSTEM_MODE(MANUAL)
+
 TCPClient client;
 byte server[] = {202,108,22,5}; // baidu
 
 void setup()
 {
-    // Make sure your SerialUSB Terminal app is closed before powering your Core
-    // Now open your SerialUSB Terminal, and hit any key to continue!
+    // Make sure your Serial Terminal app is closed before powering your Core
+    // Now open your Serial Terminal, and hit any key to continue!
     Serial.begin(115200);
     while(!Serial.available());
 
     Serial.println("connecting...");
-
     if (client.connect(server, 80))
     {
         Serial.println("connected");
@@ -37,6 +44,9 @@ void loop()
         Serial.println();
         Serial.println("disconnecting.");
         client.stop();
-        for(;;);
+        while(1) {
+            delay(1000);
+        }
     }
 }
+
--- TCP/TcpServer/src/TcpServer.ino
+++ TCP/TcpServer/src/TcpServer.ino
@@ -1,23 +1,41 @@
-TCPServer server = TCPServer(5555);
-TCPClient client,client_bak;
+/*
+    TCP Server
+    创建一个Tcp服务器, 回显tcp客户端发送的数据。
+*/
 
-char c;
+SYSTEM_MODE(MANUAL)
+
+uint16_t serverPort = 80;
+
+TCPServer server = TCPServer(serverPort);
 
 void setup()
 {
-    // start listening for clients
-    server.begin();
     Serial.begin(115200);
+    while(!WiFi.ready());
+
+    Serial.print("Local IP: ");
+    Serial.print(WiFi.localIP());
+    Serial.print(", Local Port: ");
+    Serial.println(serverPort);
+    server.begin(); // start listening for clients
+    Serial.println("TCP Server listening.");        // print a message out the serial port
 }
+
 void loop()
 {
-    if (client.connected()) {
-        // echo all available bytes back to the client
-        while (client.available()) {
-            server.write(client.read());
+    TCPClient client = server.available();   // listen for incoming clients
+    if (client) {                             // if you get a client,
+        Serial.println("New Client.");        // print a message out the serial port
+        while (client.connected()) {          // loop while the client's connected
+            while (client.available()) {      // echo all available bytes back to the client
+                char c = client.read();       // read a byte, then
+                Serial.write(c);              // print it out the serial monitor
+                client.write(c);
+            }
         }
-    } else {
-        // if no client is yet connected, check for a new connection
-        client = server.available();
+        client.stop();
+        Serial.println("Client Disconnected.");
     }
 }
+
